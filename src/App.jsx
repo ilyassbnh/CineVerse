5ced110e5a6c4b2908fc1f4731a0b7b4867cc9e4
ilyassbnh <benhida.ilyass@gmail.com>
2025-11-18 19:23:40 +0100

    install tailwind css package & update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter , Router , Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Router>
+        <Route>
+        </Route>
+      </Router>
+    </BrowserRouter>
   )
 }
 
