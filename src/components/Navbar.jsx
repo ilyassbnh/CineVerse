428abdda1f533966562e307fc15479cf28311c50
alphabdo <abdokhatib566@gmail.com>
2025-11-19 12:14:53 +0100

    nav is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom border-dark border-2">
+      <div className="container-fluid px-5">
+        <Link className="navbar-brand fw-bold fs-3 text-dark" to="/">
+          CINEVERSE
+        </Link>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/contact">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
